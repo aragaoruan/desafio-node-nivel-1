ed0b4a400c63c4f9b1c99c86891b3f2d3dfa44d2
Ruan Aragao <aragao.ruan@gmail.com>
2020-04-17 19:29:04 -0300

    Project structure

--- src/app.js
+++ src/app.js
@@ -5,8 +5,26 @@ const app = express();
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
-  res.send('Ola mundo !');
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  // TODO
+});
+
+app.post("/repositories", (request, response) => {
+  // TODO
+});
+
+app.put("/repositories/:id", (request, response) => {
+  // TODO
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  // TODO
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  // TODO
 });
 
 module.exports = app;

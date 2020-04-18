297b0542fe52844a170297641bbd024bedd88687
Ruan Aragao <aragao.ruan@gmail.com>
2020-04-17 21:26:45 -0300

    Functional project.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -7,24 +8,87 @@ app.use(express.json());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (req, res) => {
+  res.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  res.json(repository);
+
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (req, res) => {
+  
+  const { id } = req.params;
+  
+  const { title, url, techs } = req.body;
+  
+  const repositoryIndex = repositories.findIndex(repository => 
+    repository.id === id  
+  );
+
+  if (repositoryIndex < 0) {
+    return res.status(400).json({ error: 'Repository does not exists.' });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes,
+  };
+
+  repositories[repositoryIndex] = repository
+
+  return res.json(repository);
+
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex(repository => 
+    repository.id === id  
+  );
+
+  if (repositoryIndex < 0) {
+    return res.status(400).json({ error: 'Repository does not exists.' });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  res.status(204).send();
+
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params;
+
+  const repository = repositories.find(repository => 
+    repository.id === id  
+  );
+
+  if (!repository) {
+    return res.status(400).json({ error: 'Repository does not exists.' });
+  }
+
+  repository.likes += 1;
+
+  return res.json(repository);
+  
 });
 
 module.exports = app;
